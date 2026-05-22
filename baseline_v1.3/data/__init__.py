ef504d4b8da042e4673761a273db0cf41ac3bdec
jaewan31313 <jaewan3182@naver.com>
2026-05-22 10:41:07 +0900

    v1.4만 올리기 위해 v1.3 폴더 제외

--- baseline_v1.3/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dataset import PillDataset, build_dataloaders
-from .transforms import get_transforms
--- baseline_v1.3/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .coco_utils import export_predictions_coco
-from .metrics import compute_map
